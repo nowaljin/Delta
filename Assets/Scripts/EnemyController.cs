84c8e2b93d000b85e17e1a0c1866b8b5d503ca27
Nowaljin <nowaljin13@gmail.com>
2025-10-15 09:26:16 +0900

    update

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -22,13 +22,13 @@ public class EnemyController : MonoBehaviour
             Destroy(gameObject);
         }
 
-        //当たり判定
-        Vector2 p1 = transform.position;    //矢の中心座標
+        
+        Vector2 p1 = transform.position;    
         Vector2 p2 = this.player.transform.position;
         Vector2 dir = p1 - p2;
         float d = dir.magnitude;
-        float r1 = 0.5f; //矢の半径
-        float r2 = 1.0f; //プレイヤの半径
+        float r1 = 0.5f; 
+        float r2 = 1.0f; 
 
         if (d < r1 + r2)
         {
@@ -36,7 +36,7 @@ public class EnemyController : MonoBehaviour
             GameObject director = GameObject.Find("GameDirector");
             director.GetComponent<GameDirector>().DecreaseHP();
 
-            //衝突した場合は矢を消す
+            
             Destroy(gameObject);
         }
     }
